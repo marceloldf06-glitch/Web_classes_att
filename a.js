e8ae90646bf0241fdc7ac9a27282dc5b7c02bc08
Marcelo <marcelo.ldf06@aluno.ifsc.edu.br>
2026-09-23 09:01:29 -0300

    new js

---
+++ a.js
@@ -0,0 +1,11 @@
+console.log("eae mundinho achado");
+
+var nome;
+let number;
+const nPI = 3;
+console.log(typeof(nPI));
+console.log(typeof(nome));
+nome = "CheloNOW";
+console.log(typeof(nome));
+nome = 1;
+console.log(typeof(nome));
